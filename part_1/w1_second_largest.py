0ecf3c73db9845b85c9c91bf703c16b8f5cf1b46
lshostenko <luka.shostenko@gmail.com>
2024-08-19 14:20:21 +0300

    Recursive second largest.

---
+++ part_1/w1_second_largest.py
@@ -0,0 +1,15 @@
+
+def second_largest(array):
+    if len(array) == 2:
+        return min(array)
+
+    elif len(array) < 2:
+        raise ValueError('At least two elements are required.')
+
+    reduced_array = [
+        max(array[2 * i: 2 * i + 2]) for i in range(len(array) // 2)
+    ]
+    if len(array) % 2:
+        reduced_array.append(array[-1])
+
+    return second_largest(reduced_array)
