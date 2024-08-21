351581d22d31a9da0f3b1918b4783eaae0e1a020
lshostenko <luka.shostenko@gmail.com>
2024-08-21 09:40:57 +0300

    Change approach to operations on array slices.

--- part_1/w3_rselect.py
+++ part_1/w3_rselect.py
@@ -6,48 +6,48 @@ def _swap(array, ix_1, ix_2):
     array[ix_1], array[ix_2] = array[ix_2], array[ix_1]
 
 
-def _partition(array, i_min, i_max):
-    i_p = random.randrange(i_min, i_max)
+def _randpartition(array):
+    i_p = random.randrange(0, len(array))
     pivot = array[i_p]
 
-    _swap(array, i_p, i_min)
+    _swap(array, i_p, 0)
 
-    i = i_min + 1
-    j = i_min + 1
+    i_bound = 1
+    swap = False
 
-    while j < i_max and array[j] < pivot:
-        i += 1
-        j += 1
+    for i in range(1, len(array)):
+        if array[i] < pivot:
+            if swap:
+                _swap(array, i_bound, i)
 
-    while j < i_max:
-        if array[j] < pivot:
-            _swap(array, i, j)
-            i += 1
-        j += 1
+            i_bound += 1
+            continue
 
-    _swap(array, i_min, i - 1)
+        swap = True
 
-    return i - 1
+    _swap(array, 0, i_bound - 1)
+
+    return i_bound - 1
 
 
-def _rselect(array, i_min, i_max, order):
-    if i_max - i_min < 2:
-        return array[i_min]
+def _rselect(array, order):
+    if len(array) == 1:
+        return array[0]
 
-    m = _partition(array, i_min, i_max)
+    m = _randpartition(array)
 
     if m == order:
         return array[m]
 
     elif m < order:
-        return _rselect(array, m + 1, i_max, order - m - 1)
+        return _rselect(array[m + 1:], order - m - 1)
 
     else:
-        return _rselect(array, i_min, m, order)
+        return _rselect(array[:m], order)
 
 
 def rselect(array, order):
     if not 0 <= order < len(array):
         raise ValueError('Incorrect value for \'order\' parameter.')
 
-    return _rselect(copy(array), 0, len(array), order, pivot='random')
+    return _rselect(copy(array), order)
