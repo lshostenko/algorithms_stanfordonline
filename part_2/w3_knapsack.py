9d221c530db31fecbb13bffabb5f3a47ce866ffc
lshostenko <luka.shostenko@gmail.com>
2024-08-31 13:30:08 +0300

    Solution of the assignment.

--- part_2/w3_knapsack.py
+++ part_2/w3_knapsack.py
@@ -1,6 +1,11 @@
+import gzip
+import sys
+from time import monotonic
 from functools import cache
 
 import numpy as np
+
+sys.setrecursionlimit(2 ** 16)
 
 
 def _calculate_table(capacity, weights, values):
@@ -17,10 +22,14 @@ def _calculate_table(capacity, weights, values):
         w_i = weights[i - 1]
         v_i = values[i - 1]
 
+        if w_i >= capacity + 1:
+            table[i] = table[i - 1]
+            continue
+
         table[i, :w_i] = table[i - 1, :w_i]
         table[i, w_i:] = np.maximum(
             table[i - 1, w_i:],
-            table[i - 1, :capacity - w_i + 1] + v_i,
+            table[i - 1, :capacity + 1 - w_i] + v_i,
         )
 
     return table
@@ -65,11 +74,13 @@ def knapsack_max_value(capacity, weights, values):
     cur_row = np.zeros(capacity + 1, dtype=dtype)
 
     for i in range(1, num_items + 1):
-        next_row = np.zeros_like(cur_row)
-
         w_i = weights[i - 1]
         v_i = values[i - 1]
 
+        if w_i >= capacity + 1:
+            continue
+
+        next_row = np.zeros_like(cur_row)
         next_row[:w_i] = cur_row[:w_i]
         next_row[w_i:] = np.maximum(
             cur_row[w_i:],
@@ -101,3 +112,56 @@ def knapsack_max_value_recursive(capacity, weights, values):
         return value
 
     return _knapsack(capacity, len(values))
+
+
+def load_data(filename):
+    weights = []
+    values = []
+
+    if filename.endswith('.gz'):
+        open_file = gzip.open
+    else:
+        open_file = open
+
+    with open_file(filename, mode='rt') as fp:
+        capacity, n_items = map(int, fp.readline().split())
+
+        for line in fp.readlines():
+            v, w = map(int, line.split())
+            weights.append(w)
+            values.append(v)
+
+    assert len(values) == n_items
+
+    return capacity, weights, values
+
+
+if __name__ == '__main__':
+    capacity, weights, values = load_data('assets/knapsack1.txt.gz')
+
+    t0 = monotonic()
+    max_value, ixs = knapsack(capacity, weights, values)
+    t1 = monotonic()
+
+    print('Question 1 (small dataset)')
+    print(f'value:\t{max_value}')
+    print(f'items:\t{ixs}')
+    print(f'timing: {t1 - t0:.3f} s')
+
+    capacity, weights, values = load_data('assets/knapsack_big.txt.gz')
+
+    t0 = monotonic()
+    max_value = knapsack_max_value(capacity, weights, values)
+    t1 = monotonic()
+
+    print('\nQuestion 2 (large dataset), iterative approach')
+    print(f'value:\t{max_value}')
+    print(f'timing: {t1 - t0:.3f} s')
+
+    t0 = monotonic()
+    max_value = knapsack_max_value_recursive(capacity, weights, values)
+    t1 = monotonic()
+
+    print('\nQuestion 2 (large dataset), recursive approach')
+    print(f'value:\t{max_value}')
+    print(f'timing: {t1 - t0:.3f} s')
