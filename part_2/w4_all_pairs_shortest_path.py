3212735c14f0eba0a4f44596e87a8a68d6140845
lshostenko <luka.shostenko@gmail.com>
2024-09-07 11:55:29 +0300

    Add edges weight.

--- part_2/w4_all_pairs_shortest_path.py
+++ part_2/w4_all_pairs_shortest_path.py
@@ -9,12 +9,12 @@ class Graph:
         self.children = defaultdict(list)
         self.parents = defaultdict(list)
 
-    def add_edge(self, parent_node, child_node):
+    def add_edge(self, parent_node, child_node, weight):
         edge = (parent_node, child_node)
         if edge in self.edges:
             return
 
-        self.edges.add(edge)
+        self.edges[edge] = weight
         self.vertices.add(parent_node)
         self.vertices.add(child_node)
         self.children[parent_node].append(child_node)
